6db02c1af4cdeecd426d4906172d08aa04d1dd86
Tymek <lalarsek@gmail.com>
2023-05-17 09:59:08 +0200

    request handled + alerts test

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    flash('hewwo', category='error')
     return render_template("login.html")
 
 @auth.route('/logout')
